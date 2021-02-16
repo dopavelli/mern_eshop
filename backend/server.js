41623ca9c4c533d738c427e4519c4aa0012796cd
dopavelli <Sorc.2nd@gmail.com>
2021-02-16 16:58:53 +0100

    Added errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 import colors from 'colors';
 
 import productRoutes from './routes/productRoutes.js';
@@ -17,6 +19,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
