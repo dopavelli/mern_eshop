fd328b85bc27c10482a7fd194aee7bac97b8097e
dopavelli <Sorc.2nd@gmail.com>
2021-02-25 20:40:15 +0100

    Added orderController, orderRoutes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+import { addOrderItems, getOrderbyId } from '../controllers/orderController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderbyId);
+
+export default router;
